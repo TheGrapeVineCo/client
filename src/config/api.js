cd89007eb5b6c83a4e25254bb1ed713ca440add3
Jacqui <jt.cope1@gmail.com>
2022-08-02 19:42:59 +0800

    updates API connection to heroku

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // creates connection to Heroku
 const grapeVineAPI = axios.create({
-  // baseURL: "https://grapevine-rails-api.herokuapp.com",
-  baseURL: "http://localhost:4000",
+  baseURL: "https://grapevine-rails-api.herokuapp.com",
+  // baseURL: "http://localhost:4000",
 });
 
 export default grapeVineAPI;
