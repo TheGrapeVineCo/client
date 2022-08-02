de7fc6b53c214746bd98d1529432b78e888813fd
Nga Rampling <ngarampling@192-168-1-11.tpgi.com.au>
2022-08-02 22:39:53 +0930

    regular user can sign_up from the browser and data persist in local Postgresql

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,16 @@ import axios from "axios";
 
 // creates connection to Heroku
 const grapeVineAPI = axios.create({
-  baseURL: "https://grapevine-rails-api.herokuapp.com",
+  baseURL: "http://localhost:4000"
 });
+
+grapeVineAPI.interceptors.request.use(request => {
+  const token = sessionStorage.getItem("token")
+  console.log(token)
+  if (token) {
+    request.headers["Authorization"] = `Bearer ${token}`
+  }
+  return request
+})
 
 export default grapeVineAPI;
--- src/services/authServices.js
+++ src/services/authServices.js
@@ -2,7 +2,10 @@ import grapeVineAPI from '../config/api';
 
 // data passed in from user signup from state
 export async function signUp(data) {
-  const response = await grapeVineAPI.post("api/sign_up", data);
+  // data needs to be stored inside user: as devise gem from Rails requires to data its data in this way
+  const data_for_devise = {user: data}
+  const response = await grapeVineAPI.post("api/sign_up", data_for_devise);
   console.log(response.data);
+
   return response.data;
 }
