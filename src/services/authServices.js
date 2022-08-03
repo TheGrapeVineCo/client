a0d1d82452b2640b242930fad5f2d2fd16e88737
Nga Rampling <ngarampling@192-168-1-11.tpgi.com.au>
2022-08-03 15:36:37 +0930

    changes in the authServices to store jwt

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,19 +1,22 @@
-import grapeVineAPI from '../config/api';
+import grapeVineAPI from "../config/api";
 
 // data passed in from user signup from state
 export async function signUp(data) {
   // data needs to be stored inside user: as devise gem from Rails requires to data its data in this way
-  const data_for_devise = {user: data}
-  const response = await grapeVineAPI.post("api/sign_up", data_for_devise);
+  const payload = { user: { ...data } };
+  console.log(payload);
+  const response = await grapeVineAPI.post("api/sign_up", payload);
   console.log(response.data);
 
   return response.data;
 }
 
+// facilitates user login to FE/BE
 export async function login(data) {
   // data needs to be stored inside user: as devise gem from Rails requires to data its data in this way
-  const data_for_devise = {user: data}
-  const response = await grapeVineAPI.post("api/login", data_for_devise);
+  const payload = { user: { ...data } };
+  console.log(payload);
+  const response = await grapeVineAPI.post("api/login", payload);
   console.log(response.data);
 
   return response.data;
