046bb2f57a865a8d911030cab145504dea217fbe
Nga Rampling <ngarampling@192-168-1-11.tpgi.com.au>
2022-08-04 23:02:33 +0930

    removes old comments

--- src/config/api.js
+++ src/config/api.js
@@ -9,38 +9,10 @@ const grapeVineAPI = axios.create({
   // fetching API from Heroku
   // baseURL: "https://grapevine-rails-api.herokuapp.com/",
 
-  //This worked. Request headers maintain Auth Bearer token during login sessions for wine_listings pages.
+  // Request headers maintain Auth Bearer token during login sessions for wine_listings pages.
   headers: {
     Authorization : sessionStorage.getItem("token")
     }
 });
 
 export default grapeVineAPI;
-// Request interceptors for API calls
-
-// This axios interceptors.request hasn't worked in storing jwt into sessionStorage.
-// See codes above for storing jwt in sessionStorage instead.
-// grapeVineAPI.interceptors.request.use(
-//   config => {
-//     const token = sessionStorage.getItem("token")
-//     console.log(token)
-//     if (token === !undefined)
-//       config.headers["Authorization"] = sessionStorage.getItem("token");
-//         return config;
-//       },
-//       error => {
-//         return Promise.reject(error);
-//       }
-// );
-
-
-// grapeVineAPI.interceptors.request.use((request) => {
-//   const token = sessionStorage.getItem("token")
-  
-  // if (token === !undefined) {
-  //   request.headers["Authorization"] = `Bearer ${token}`
-  // }
-//   return request
-// })
-
-
