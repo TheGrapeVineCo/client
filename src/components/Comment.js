32927938316f87c9be268281e94afa412fe794e6
Jacqui <jt.cope1@gmail.com>
2022-08-05 17:48:44 +0800

    refactors Comment component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,35 +1,30 @@
 import React from "react";
 import { useGlobalState } from "../utils/stateContext";
 import Button from "react-bootstrap/Button";
-// import { removeComment } from "../services/commentServices";
 
 // Renders one comment from 'Comments' component
 const Comment = ({ comment }) => {
   const { store } = useGlobalState();
   const { loggedInUser } = store;
-  // date format not correct - may update to include
-  // const timePassed = new Date() - new Date(comment.updated);
-  // const currentUsername = "test";
 
   const deleteComment = (e) => {
     e.preventDefault();
-    console.log(comment);
+    // TODO: implement delete comment
   };
+
+  const { username, updated, comment: text, user_id } = comment;
 
   return (
     <>
-      <p className="text-detail">{comment.username}</p>
-      {/* <p className="text-detail">{currentUsername}</p> */}
-      <p className="text-detail">{comment.updated}</p>
-      <p>{comment.comment}</p>
-      {loggedInUser === comment.user_id && (
-        // <Button onClick={""}>Edit Message</Button>
-        <Button variant="link" className="custom-btn" comment={comment} id={55}>
+      <p className="text-detail">{username}</p>
+      <p className="text-detail">{updated}</p>
+      <p>{text}</p>
+      {loggedInUser === user_id && (
+        <Button variant="link" className="custom-btn" comment={comment}>
           Edit Message
         </Button>
       )}
-      {loggedInUser === comment.user_id && (
-        // <Button onClick={""}>Delete Message</Button>
+      {loggedInUser === user_id && (
         <Button
           variant="link"
           className="custom-btn-delete"
