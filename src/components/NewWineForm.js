a3a3cf504c7e93907530584f54292df7e7c853c0
Jacqui <jt.cope1@gmail.com>
2022-08-05 18:21:15 +0800

    refactors newWineForm

--- src/components/NewWineForm.js
+++ src/components/NewWineForm.js
@@ -1,5 +1,4 @@
 import Button from "react-bootstrap/Button";
-// import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
@@ -7,11 +6,12 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGlobalState } from "../utils/stateContext";
 
-// somehow need to pass eg. "loggedInAdmin" param so only admins can fill out this form
+// TODO: need to pass a "loggedInAdmin" param so only admins can fill out this form
 function NewWineForm() {
   const { store, dispatch } = useGlobalState();
   const { wineListings } = store;
   const navigate = useNavigate();
+
   // Sets initial fields as empty
   const initialFormData = {
     brand: "",
@@ -51,7 +51,7 @@ function NewWineForm() {
 
   const [formData, setFormData] = useState(initialFormData);
 
-  //   obtains data from new wine listing
+  // obtains data from new wine listing
   const handleFormData = (e) => {
     setFormData({
       ...formData,
@@ -59,7 +59,7 @@ function NewWineForm() {
     });
   };
 
-  //   needs validation to ensure empty data is submit
+  // needs validation to ensure empty data is submit
   const handleSubmit = (e) => {
     e.preventDefault();
     addNewWineListing(formData);
@@ -68,80 +68,81 @@ function NewWineForm() {
     navigate("/wineListings");
   };
 
-  //   clears the form data for next entry
+  // clears the form data for next entry
   const clearFormData = () => {
     setFormData(initialFormData);
   };
 
+  const { Group, Label, Control } = Form;
   return (
     <>
       <h2 className="page-title">Create a New Wine Listing 🍷</h2>
       <Form onSubmit={handleSubmit} className="cwl-container ">
         <Row>
           <Col lg={6} md={12} sm={12} xs={12}>
-            <Form.Group className="mb-3" controlId="brand">
-              <Form.Label className="text-detail">Brand</Form.Label>
-              <Form.Control
+            <Group className="mb-3" controlId="brand">
+              <Label className="text-detail">Brand</Label>
+              <Control
                 type="string"
                 placeholder="Enter wine brand"
                 value={formData.brand}
                 onChange={handleFormData}
               />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="grape_variety">
-              <Form.Label className="text-detail">Grape Variety</Form.Label>
-              <Form.Control
+            </Group>
+            <Group className="mb-3" controlId="grape_variety">
+              <Label className="text-detail">Grape Variety</Label>
+              <Control
                 type="string"
                 placeholder="Shiraz, Merlot, Sauvignon Blanc..."
                 value={formData.grape_variety}
                 onChange={handleFormData}
               />
-            </Form.Group>
+            </Group>
 
-            <Form.Group className="mb-3" controlId="year">
-              <Form.Label className="text-detail">Year</Form.Label>
-              <Form.Control
+            <Group className="mb-3" controlId="year">
+              <Label className="text-detail">Year</Label>
+              <Control
                 type="integer"
                 placeholder="Year"
                 value={formData.year}
                 onChange={handleFormData}
               />
-            </Form.Group>
+            </Group>
           </Col>
 
           <Col lg={6} md={12} sm={12} xs={12}>
-            <Form.Group className="mb-3" controlId="category">
-              <Form.Label className="text-detail">Category</Form.Label>
-              <Form.Control
+            <Group className="mb-3" controlId="category">
+              <Label className="text-detail">Category</Label>
+              <Control
                 type="string"
                 placeholder="Red, White, Sparkling..."
                 value={formData.category}
                 onChange={handleFormData}
               />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="country">
-              <Form.Label className="text-detail">Country</Form.Label>
-              <Form.Control
+            </Group>
+            <Group className="mb-3" controlId="country">
+              <Label className="text-detail">Country</Label>
+              <Control
                 type="string"
                 placeholder="Australia"
                 value={formData.country}
                 onChange={handleFormData}
               />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="region">
-              <Form.Label className="text-detail">Region</Form.Label>
-              <Form.Control
+            </Group>
+            <Group className="mb-3" controlId="region">
+              <Label className="text-detail">Region</Label>
+              <Control
                 type="string"
                 placeholder="Barossa Valley, Margaret River..."
                 value={formData.region}
                 onChange={handleFormData}
               />
-            </Form.Group>
+            </Group>
           </Col>
-          <Form.Group className="mb-3" controlId="description">
+          <Group className="mb-3" controlId="description">
             <Col lg={6} md={12} sm={12} xs={12}>
-              <Form.Label className="text-detail">Description</Form.Label>
-              <Form.Control
+              <Label className="text-detail">Description</Label>
+              <Control
                 as="textarea"
                 rows={3}
                 placeholder="Describe the wine..."
@@ -150,9 +151,9 @@ function NewWineForm() {
                 className={"desc-textarea"}
               />
             </Col>
-          </Form.Group>
+          </Group>
         </Row>
-        <Form.Group className="button-form">
+        <Group className="button-form">
           <Button
             className="btn-default button-mob"
             variant="primary"
@@ -160,7 +161,7 @@ function NewWineForm() {
           >
             Submit
           </Button>
-        </Form.Group>
+        </Group>
       </Form>
     </>
   );
