ed209f650c82a435f44e90d7a1d9853eb47ee425
Jacqui <jt.cope1@gmail.com>
2022-08-12 18:54:45 +0800

    updates headers to include token

--- src/config/api.js
+++ src/config/api.js
@@ -3,16 +3,21 @@ import axios from "axios";
 // creates connection to Heroku
 const grapeVineAPI = axios.create({
   // fetching API from local laptop
-
   // baseURL: "http://localhost:4000",
 
   // fetching API from Heroku
   baseURL: "https://grapevine-rails-api.herokuapp.com/",
+});
 
-  // Request headers maintain Auth Bearer token during login sessions for wine_listings pages.
-  headers: {
-    Authorization: sessionStorage.getItem("token"),
-  },
+// enables user to perform authorized crud actions
+grapeVineAPI.interceptors.request.use((req) => {
+  // sends token in related request
+  const token = JSON.parse(sessionStorage.getItem("token"));
+
+  if (token) {
+    req.headers["Authorization"] = `${token}`;
+  }
+  return req;
 });
 
 export default grapeVineAPI;
